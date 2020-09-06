14c06daf7529e46a07d02eabc1f95d80ae897a16
Saher <saher.suga@gmail.com>
2020-09-06 12:08:58 -0500

    Collision detections

--- flabby_Birds.py
+++ flabby_Birds.py
@@ -21,16 +21,18 @@ bg=pygame.transform.scale2x(bg)
 
 bottom_surface=pygame.image.load("base.png").convert()
 bottom_surface=pygame.transform.scale2x(bottom_surface)
+bottom_surface_rect=bottom_surface.get_rect(topleft=(0,912))
 bottom_x_pos=0
 
 pipe_surface=pygame.image.load("pipe-green.png").convert()
 pipe_surface=pygame.transform.scale2x(pipe_surface)
 pipe_surface_flipped=pygame.transform.flip(pipe_surface, False, True)
-pipe_rect=pipe_surface.get_rect(center=((576//2),(1024//2)))
+pipe_rect=pipe_surface.get_rect(topleft=(600,0))
+pipe_rect_flipped=pipe_surface_flipped.get_rect(topleft=((600,400)))
 
 BirdSprite = mid_surface=pygame.image.load("bluebird-midflap.png").convert()
 mid_surface=pygame.transform.scale2x(mid_surface)
-bird_rect=mid_surface.get_rect(center=((576//2),(1024//2)))
+bird_rect=mid_surface.get_rect(center = (100,512))
 
 # list of pregenerated pipes for user to jump through
 list_of_pipe_dimensions = [random.randint(400,460) for i in range(1,1000)]
@@ -43,12 +45,6 @@ def Bird():
     ##pygame.draw.rect(screen, (255,255,255), (x_pos,y_pos,width,height))
      screen.blit(mid_surface,(bird_rect))
 
-#def Pipe():
-    #screen.blit(pipe_surface_flipped,(300,-500))
-    #screen.blit(pipe_surface,(300,500))
-    #pipes=[]
-    #PIPESPAWNER=pygame.USEREVENT ##event that works with no triggers
-    #pygame.time.set_timer(PIPESPAWNER,1000)
 def Pipe_merge():
     screen.blit(pipe_surface,(pipe_x_pos,pipe_y_pos))
     screen.blit(pipe_surface_flipped,(pipe_x_pos,pipe_y_pos * -1))
@@ -56,6 +52,10 @@ def Pipe_merge():
     
 def Create_a_pipe():
     return pipe_rect
+    
+#def Collisions(pipe_rect):
+    #for pipe_surface in pipe_rect
+    
     
 
 def background():
@@ -81,30 +81,41 @@ if __name__ == "__main__":
             bird_rect.top = 0
         if bird_rect.bottom > 850:  # try not to have value hardcoded
             bird_rect.bottom = 850
+            print("Colliding")
+            #print(bird_rect.colliderect(bottom_surface_rect))
+
+            #colliding with floor
+
         
         background()
         Bird()
-        #Pipe()
         Pipe_merge()
-        #if event.type == PIPESPAWNER:
-        #   pipes.append(Create_a_pipe())
-        #   print (pipes)
         bottom_x_pos-=1
         pipe_x_pos-=3
+        pipe_rect.x = pipe_x_pos
+        pipe_rect_flipped.x = pipe_x_pos
+        pipe_rect.y = pipe_y_pos
+        pipe_rect_flipped.y = pipe_y_pos * -1
+        pygame.draw.rect(screen, [0,0,0], pipe_rect)
+        pygame.draw.rect(screen, [255,0,0], pipe_rect_flipped)
+        #pipe_rect.x-=3
+        #pipe_rect_flipped.x-=3
+    
         if bottom_x_pos <= (bottom_width *-1):
             bottom_x_pos=0
       
         if pipe_x_pos <=-100: 
             newYPosition = random.choice(list_of_pipe_dimensions)
             pipe_x_pos = 600
+            pipe_rect.x = 600
+            pipe_rect_flipped.x = 600
             pipe_y_pos = newYPosition
             currentScore+=1
             print(f"Current Pipe Dimensions: {pipe_x_pos} x {newYPosition}")
             print(f"User's current score: {currentScore}")
-        
+        #print(bird_rect.colliderect(pipe_rect_flipped))
+        #print(bird_rect.colliderect(pipe_rect or pipe_rect_flipped ))
         Floor_merge()
-
-
         pygame.display.update()
         clock.tick(120)
        
